120445be3b2a294058942adddba26b025e3adece
wenay12 <wenayr@gmail.com>
2023-08-03 12:16:10 +0300

    12

--- src/addres.ts
+++ src/addres.ts
@@ -1,7 +1,7 @@
 export const aFont = "/home/alex_andr/pdf/resource/fonts/"
 export const aExcel = ""
 export const aImage = ""
-export const aResult = ""
+export const aResult = "/AGTY/www/pro.usavto.soft/content/files/print/waybills/tmp/";
 
 
 // export const aFont = "../resource/fonts/"
