cb6481eefe9c0ff920889af7674e61e838e5ba68
wenay12 <wenayr@gmail.com>
2023-08-18 14:23:31 +0300

    12

--- src/addres.ts
+++ src/addres.ts
@@ -1,10 +1,10 @@
-// export const aFont = "/home/alex_andr/pdf/resource/fonts/"
-// export const aExcel = ""
-// export const aImage = ""
-// export const aResult = "/AGTY/www/pro.usavto.soft/content/files/print/waybills/tmp/";
+export const aFont = "/home/alex_andr/pdf/resource/fonts/"
+export const aExcel = ""
+export const aImage = ""
+export const aResult = "/AGTY/www/pro.usavto.soft/content/files/print/waybills/tmp/";
 
 
-export const aFont = "../resource/fonts/"
-export const aExcel = "../resource/excel/"
-export const aImage = "../resource/image/"
-export const aResult = "../resource/result/"
+// export const aFont = "../resource/fonts/"
+// export const aExcel = "../resource/excel/"
+// export const aImage = "../resource/image/"
+// export const aResult = "../resource/result/"
